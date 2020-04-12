c601107423262137ca8aa33f33a1c35463de4ec1
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-04-12 13:13:41 -0500

    initial codes

---
+++ assets/js/script.js
@@ -0,0 +1,202 @@
+
+var startButton = document.getElementById('start-btn');
+var mainPage = 'start';
+var questionContainerEl = document.getElementById('question-container');
+var questionEl = document.getElementById('question');
+var answerEl = document.getElementById('answer-buttons');
+var answerClicked = document.getElementById('answer');
+var scorePage = document.getElementById('score');
+var displayScoreVal = document.getElementById('total-score');
+var totalScore = 0;
+var timeLeft = 75;
+var timerEl = document.getElementById("countdown");
+var timerElzero = document.getElementById("countdown-0");
+var correctAnswer = document.getElementById('correct');
+var wrongAnswer = document.getElementById('wrong');
+var initials = document.getElementById('initials');
+var initialsEntered = document.getElementById('initials-entered');
+var submitScore = document.getElementById("submit-btn");
+var highScores = JSON.parse(localStorage.getItem("highScores")) || []; // get the score, or the initial value if empty 
+const MAX_HIGH_SCORES = 5;
+
+
+function startGame() {
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        timerEl.textContent = timeLeft;
+        
+    if (timeLeft === 0) {
+        displayScore();
+        clearInterval(timeInterval);
+    }
+
+}, 1000);
+
+    startButton.classList.add('hide'); //hides start button once clicked
+    document.getElementById(mainPage).style.visibility = "hidden"; //hides mainPage text once start is clicked
+    currentQuestionIndex = 0;
+    questionContainerEl.classList.remove('hide');
+    setNextQuestion();
+}
+
+function setNextQuestion() {
+    resetState();
+    showQuestion(questions[currentQuestionIndex]);
+}
+
+function displayScore () {
+    resetState();
+    questionEl.classList.add('hide');
+    timerEl.classList.add('hide');
+    timerElzero.classList.remove('hide');
+    displayScoreVal.textContent = totalScore;
+    scorePage.classList.remove('hide');
+
+    localStorage.setItem('mostRecentScore', totalScore);
+
+    //hide final "Correct/Wrong" result once player starts to enter initials
+    initials.addEventListener('keyup', () => {
+        wrongAnswer.classList.add('hide');
+        correctAnswer.classList.add('hide');
+    }); 
+
+
+var mostRecentScore = localStorage.getItem('mostRecentScore');
+
+submitScore.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    const score = {
+        score: mostRecentScore,
+        initials: initials.value
+    };
+    //debugger;
+    if (initials.value === "") {
+        window.alert("Initials cannot be blank");
+    } else {
+        highScores.push(score);
+
+        highScores.sort((a, b) => b.score - a.score); //sorts highscores in order
+        highScores.splice(5); //keeps top 5 scores in highscore local storage
+        
+        localStorage.setItem("highScores", JSON.stringify(highScores));
+        window.location.assign("highscores.html")
+    }
+});
+
+initialsEntered.reset();
+
+}
+    
+function showQuestion(question) {
+    //debugger;
+    if (currentQuestionIndex < questions.length) {
+    questionEl.innerText = question.question;
+    Array.from(question.answers).forEach(answer => {
+        var button = document.createElement ('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerEl.appendChild(button);
+    });
+    } else {
+        displayScore();
+    }
+}
+
+function resetState() {
+
+    while (answerEl.firstChild) {
+        answerEl.removeChild(answerEl.firstChild);
+    }
+}
+function selectAnswer(e) {
+
+    var selectedBotton = e.target;
+    var correct = selectedBotton.dataset.correct;
+
+    if (correct) {
+        totalScore += 10;
+        correctAnswer.classList.remove('hide');
+        wrongAnswer.classList.add('hide');
+    } else {
+        wrongAnswer.classList.remove('hide');
+        correctAnswer.classList.add('hide');
+        if(timeLeft >= 15) {
+            timeLeft -= 15;
+        } else {
+            timeLeft = 0;
+        }
+        if(totalScore >= 5) {
+            totalScore -= 5;
+        } else {
+            totalScore = 0;
+        }
+    }
+}
+// replace previous question/answer set as long as there are more questions to ask
+startButton.addEventListener('click', startGame);
+/*function nextQuestion() {
+    if (currentQuestionIndex < questions.length) {
+        currentQuestionIndex++;
+        setNextQuestion();
+    }
+}*/
+answerEl.addEventListener('click', function() {
+    if (currentQuestionIndex < questions.length) {
+        currentQuestionIndex++;
+        setNextQuestion();
+    }
+}); 
+
+// The array of questions for our quiz game.
+var questions = [
+    {
+        question: 'Commonly used data types DO NOT include:',
+        answers: [
+            { text: 'strings', correct: false },
+            { text: 'booleans', correct: false },
+            { text: 'alerts', correct: false },
+            { text: 'numbers', correct: true },
+        ]
+    },
+    {
+        question: 'The condition in an if/else statement is enclosed with ______.',
+        answers: [
+            { text: 'quotes', correct: false },
+            { text: 'curly brackets', correct: false },
+            { text: 'parenthesis', correct: true  },
+            { text: 'square brackets', correct: false },
+        ]
+    },
+    {
+        question: 'Arrays in JavaScript can be used to store ______.',
+        answers: [
+            { text: 'numbers and strings', correct: false },
+            { text: 'other arrays', correct: false },
+            { text: 'booleans', correct: false },
+            { text: 'all of the above', correct: true },
+        ]
+    },
+    {
+        question: 'String values must be enclosed within ______ when being assigned to variables.',
+        answers: [
+            { text: 'commas', correct: false },
+            { text: 'curly brackets', correct: false },
+            { text: 'quotes', correct: true  },
+            { text: 'parenthesis', correct: false },
+        ]
+    },
+    {
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        answers: [
+            { text: 'JavaScript', correct: false },
+            { text: 'terminal/bash', correct: false },
+            { text: 'for loops', correct: false },
+            { text: 'console.log', correct: true },
+        ]
+    }
+];
